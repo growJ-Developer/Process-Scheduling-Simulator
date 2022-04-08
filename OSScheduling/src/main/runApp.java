b7883b5d97e8f67895f28fb749dbfc87025f852e
byj2102 <addps5012@kakao.com>
2022-04-08 23:02:51 +0900

    -initial Commit 22.04.08

---
+++ OSScheduling/src/main/runApp.java
@@ -0,0 +1,10 @@
+package main;
+
+public class runApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ OSScheduling/src/scheduling/scheduling.java
@@ -0,0 +1,5 @@
+package scheduling;
+
+public abstract class scheduling {
+
+}
